769f1cf1ed8fdfd8a8a1661239184b3bc15b812d
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-06-10 20:11:27 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('datejs')
 
-
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
+  args.forEach((arg) => {
+    combinedObject.users.push(...arg);
+  });
+  combinedObject.merge_date = Date.parse(new Date).toString("M/d/yyyy")
+  return combinedObject
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
